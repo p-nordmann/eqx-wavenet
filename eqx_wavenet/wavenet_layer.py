51f2de4d667ccefa580db46d13ac41bc3e8552c4
Pierre-Louis Nordmann <pierrelouis.nordmann@gmail.com>
2023-12-03 15:07:46 +0000

    feat(experimental): implemented pre-gated convolutions
    + defined homemade pointwise convolutions

--- eqx_wavenet/wavenet_layer.py
+++ eqx_wavenet/wavenet_layer.py
@@ -2,6 +2,7 @@ from typing import TypeVar
 
 import equinox as eqx
 import jax
+import jax.numpy as jnp
 from beartype import beartype
 from jaxtyping import Array, Float32, PRNGKeyArray, jaxtyped
 
@@ -11,40 +12,75 @@ T = TypeVar("T")
 Pair = tuple[T, T]
 
 
-def dilated_conv(
-    size_layers: int,
-    dilation: int,
-    *,
-    key: PRNGKeyArray,
-) -> eqx.nn.Conv1d:
-    return eqx.nn.Conv1d(
-        in_channels=size_layers,
-        out_channels=size_layers,
-        kernel_size=2,
-        dilation=dilation,
-        padding=[(dilation, 0)],  # We try to preserve the same length as input.
-        key=key,
-    )
+class PointWiseConv(eqx.Module):
+    """Pointwise 1d convolution without bias."""
+
+    weight: Array
+    padding: tuple[int, int] | None = eqx.field(static=True)
+
+    def __init__(
+        self,
+        size_in: int,
+        size_out: int,
+        padding: tuple[int, int] | None = None,
+        *,
+        key: PRNGKeyArray,
+    ):
+        self.weight = jnp.sqrt(2 / size_out) * jax.random.normal(
+            key=key, shape=(size_out, size_in, 1)
+        )
+        self.padding = padding
+
+    def __call__(
+        self, x: Float32[Array, " size_in time"]
+    ) -> Float32[Array, " size_out _"]:
+        return jnp.squeeze(
+            jax.lax.conv_general_dilated(
+                lhs=jnp.expand_dims(x, axis=0),
+                rhs=self.weight,
+                window_strides=[1],
+                padding=[self.padding] if self.padding else [(0, 0)],
+            ),
+            axis=0,
+        )
 
 
-def pointwise_conv(
-    size_layers: int,
-    *,
-    key: PRNGKeyArray,
-) -> eqx.nn.Conv1d:
-    return eqx.nn.Conv1d(
-        in_channels=size_layers,
-        out_channels=size_layers,
-        kernel_size=1,
-        key=key,
-    )
+class PreGatedConv(eqx.Module):
+    conv_1: PointWiseConv
+    conv_2: PointWiseConv
+    conv_gate: PointWiseConv
+    gate_bias: Array
+    total_bias: Array
+    dilation: int = eqx.field(static=True)
+
+    def __init__(
+        self, size_in: int, size_out: int, dilation: int, *, key: PRNGKeyArray
+    ):
+        keys = jax.random.split(key, 3)
+        self.conv_1 = PointWiseConv(size_in, size_out, (dilation, 0), key=keys[0])
+        self.conv_2 = PointWiseConv(size_in, size_out, (dilation, 0), key=keys[1])
+        self.conv_gate = PointWiseConv(size_in, size_out, (dilation, 0), key=keys[2])
+        self.gate_bias = jnp.zeros((size_out, 1))
+        self.total_bias = jnp.zeros((size_out, 1))
+        self.dilation = dilation
+
+    def __call__(
+        self, x: Float32[Array, " size_in time"]
+    ) -> Float32[Array, " size_out time"]:
+        gate = jax.nn.sigmoid(self.conv_gate(x) + self.gate_bias)
+        z_1 = self.conv_1(x)
+        z_2 = self.conv_2(x)
+        return jnp.tanh(
+            z_1[:, self.dilation :] * gate[:, self.dilation :]
+            + z_2[:, : -self.dilation] * gate[:, : -self.dilation]
+            + self.total_bias
+        )
 
 
 class WavenetLayer(eqx.Module):
-    filter_conv: eqx.nn.Conv1d
-    gate_conv: eqx.nn.Conv1d
-    residual_conv: eqx.nn.Conv1d
-    skip_conv: eqx.nn.Conv1d
+    pre_gated_conv: PreGatedConv
+    residual_conv: PointWiseConv
+    skip_conv: PointWiseConv
     norm: RMSLayerNorm
 
     def __init__(
@@ -54,14 +90,12 @@ class WavenetLayer(eqx.Module):
         *,
         key: PRNGKeyArray,
     ):
-        key_1, key_2, key_3, key_4 = jax.random.split(key, 4)
-
-        self.filter_conv = dilated_conv(size_layers, dilation, key=key_1)
-        self.gate_conv = dilated_conv(size_layers, dilation, key=key_2)
-
-        self.residual_conv = pointwise_conv(size_layers, key=key_3)
-        self.skip_conv = pointwise_conv(size_layers, key=key_4)
-
+        keys = jax.random.split(key, 3)
+        self.pre_gated_conv = PreGatedConv(
+            size_layers, size_layers, dilation, key=keys[0]
+        )
+        self.residual_conv = PointWiseConv(size_layers, size_layers, key=keys[1])
+        self.skip_conv = PointWiseConv(size_layers, size_layers, key=keys[2])
         self.norm = RMSLayerNorm(size_layers)
 
     @jaxtyped
@@ -71,7 +105,5 @@ class WavenetLayer(eqx.Module):
         x: Float32[Array, " size_layers time"],
     ) -> Pair[Float32[Array, " size_layers time"]]:
         x_normalized = jax.vmap(self.norm, in_axes=1, out_axes=1)(x)
-        z = jax.nn.tanh(self.filter_conv(x_normalized)) * jax.nn.sigmoid(
-            self.gate_conv(x_normalized)
-        )
+        z = self.pre_gated_conv(x_normalized)
         return self.residual_conv(z) + x, self.skip_conv(z)
